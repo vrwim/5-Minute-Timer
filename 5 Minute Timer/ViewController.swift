3378a5aa8edd45ca8120eb1762c0edc9bb277d79
Wim Van Renterghem <git@vrwim.com>
2019-11-15 12:16:02 +0100

    Initial Commit

---
+++ 5 Minute Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5 Minute Timer
+//
+//  Created by Wim Van Renterghem on 15/11/2019.
+//  Copyright © 2019 Bazookas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ 5 Minute TimerTests/__Minute_TimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  __Minute_TimerTests.swift
+//  5 Minute TimerTests
+//
+//  Created by Wim Van Renterghem on 15/11/2019.
+//  Copyright © 2019 Bazookas. All rights reserved.
+//
+
+import XCTest
+@testable import __Minute_Timer
+
+class __Minute_TimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
